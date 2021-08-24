3b0d6a9119d8e500f733fe48bd042fa65c7c6886
JuanAntonaccio <jmantonaccio@gmail.com>
2021-08-24 01:01:00 +0000

    subo ejer 23

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,4 +1,18 @@
-
+const rep="let it be, ";
+const rep2="let it be";
+let resu="";
+function sing(){
+  for (let i=1;i<5;i++){
+      resu=resu+rep;
+  }
+  resu=resu+" whisper words of wisdom, ";
+  for (let i=1;i<6;i++){
+      resu=resu+rep;
+}
+resu=resu+" there will be an answer, ";
+resu=resu+rep2;
+return resu;
+}
 
 //Your code above ^^^
 
