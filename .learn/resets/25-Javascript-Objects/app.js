31ade399f9b748b223a904e72f2bf2c637634e6f
JuanAntonaccio <jmantonaccio@gmail.com>
2021-08-25 15:20:32 +0000

    con el 25 con error

--- .learn/resets/25-Javascript-Objects/app.js
+++ .learn/resets/25-Javascript-Objects/app.js
@@ -18,19 +18,13 @@ var person2 = {
 
 var family = {
     lastname: "Doe",
-    members: [person, person2,person3]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2]   //Array of objects, don't forget to add Jimmy
 };
 
 
 function addAllFamilyLuckyNumbers(anArray){
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-  for (let i=0; i<anArray.length;i++ ){
-      let nuevoarr=anArray[i].lucky_numbers;
-      console.log(nuevoarr);
-      //for(let j=0;j<nuevoarr.length;j++){
-      //     sumOfAllLuckyNumbers+=nuevoarr[j];
-      // }
-  }
+
   //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
   
@@ -38,16 +32,7 @@ function addAllFamilyLuckyNumbers(anArray){
 }
 
 //Enter all your code here:
-person.lucky_numbers[3]=33;
 
-var person3 = {
-    name: "Jimmy",
-    lastname: "Doe",
-    age: 13,
-    gender: "male",
-    lucky_numbers: [ 1, 2, 3, 4],
-    significant_other: null
-};
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
